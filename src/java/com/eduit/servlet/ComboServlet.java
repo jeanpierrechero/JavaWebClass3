9f9d3e3fdf0dd84545d03e9f00a72f41db857e9f
educacionit <educacionit@alumno-1003-17>
2017-09-09 11:33:29 -0300

    javawebclass3

--- src/java/com/eduit/servlet/ComboServlet.java
+++ src/java/com/eduit/servlet/ComboServlet.java
@@ -11,13 +11,8 @@ package com.eduit.servlet;
  */
 
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
