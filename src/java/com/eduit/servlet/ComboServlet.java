456d077f911e11e56093c7e0e6502ad2547b62d3
educacionit <educacionit@alumno-1003-17>
2017-09-09 11:30:55 -0300

    javawebclass3

---
+++ src/java/com/eduit/servlet/ComboServlet.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eduit.servlet;
+
+/**
+ *
+ * @author educacionit
+ */
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name="cs", urlPatterns="/add")
+public class ComboServlet extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String nombre = request.getParameter("nom");
+        HttpSession session = request.getSession();
+        List<String>nombres=(List<String>)session.getAttribute("noms");
+            if(nombres==null){
+                nombres = new ArrayList<>();
+                session.setAttribute("noms",nombres);
+            }
+            nombres.add(nombre);
+            response.sendRedirect("index.jsp");
+        
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,29 @@
+<%-- 
+    Document   : index
+    Created on : 09-sep-2017, 10:46:15
+    Author     : educacionit
+--%>
+
+<%@page import="java.util.List"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form method="POST" action="add">
+            nombre:<input type="text" name="nom"/>
+            <input type="submit" value="agregar"/>
+            </<form>
+                <%List<String> nombres = (List<String>) session.getAttribute("noms");%>
+                <select>
+                    <%if (nombres != null) {%>
+                    <%for (String nom : nombres) {%>
+                    <option><%=nom%></option>
+                    <%}%>
+                    <%}%>
+                </select>
+                </body>
+                </html>
